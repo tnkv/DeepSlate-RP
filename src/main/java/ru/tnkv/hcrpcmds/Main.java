c962a912296c05b76e018bd182fdf4a0affe3767
Дмитрий <zoga.debian@gmail.com>
2023-01-15 22:30:11 +0300

    Небольшой рефактор.

--- src/main/java/ru/tnkv/hcrpcmds/Main.java
+++ src/main/java/ru/tnkv/hcrpcmds/Main.java
@@ -1,35 +1,45 @@
 package ru.tnkv.hcrpcmds;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import ru.tnkv.hcrpcmds.commands.DiceCommand;
 import ru.tnkv.hcrpcmds.commands.MeCommand;
 import ru.tnkv.hcrpcmds.commands.TodoCommand;
 import ru.tnkv.hcrpcmds.commands.TryCommand;
-
 import java.io.File;
+import java.util.logging.Level;
 
 public final class Main extends JavaPlugin {
+    public static FileConfiguration config;
 
     @Override
     public void onEnable() {
-        File folder = new File(this.getDataFolder() + "/");
-        if(!folder.exists()) {
-            folder.mkdir();
-        }
-        File config_file = new File(this.getDataFolder() + "/config.yml");
-        if(!config_file.exists()) {
-            this.saveResource("config.yml", true);
-        }
+        this.saveResource("config.yml", false);
+
+        config = YamlConfiguration.loadConfiguration(new File(this.getDataFolder() + "/config.yml"));
 
         this.getCommand("dice").setExecutor(new DiceCommand());
         this.getCommand("try").setExecutor(new TryCommand());
         this.getCommand("me").setExecutor(new MeCommand());
         this.getCommand("todo").setExecutor(new TodoCommand());
+
+        this.getLogger().log(Level.INFO, "Включено.");
     }
 
     @Override
     public void onDisable() {
+        this.getLogger().log(Level.INFO, "Выключено.");
+    }
 
+    public static void sendMessageToNear(Component message, Player player, int radius) {
+        for (Player near : Bukkit.getOnlinePlayers()) {
+            if (near.getWorld() == player.getWorld() && near.getLocation().distance(player.getLocation()) <= radius) {
+                near.sendMessage(message);
+            }
+        }
     }
 }
--- src/main/java/ru/tnkv/hcrpcmds/commands/TryCommand.java
+++ src/main/java/ru/tnkv/hcrpcmds/commands/TryCommand.java
@@ -3,44 +3,27 @@ package ru.tnkv.hcrpcmds.commands;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
-import java.util.Objects;
+import ru.tnkv.hcrpcmds.Main;
 
 public class TryCommand implements CommandExecutor {
-    private final FileConfiguration config = org.bukkit.configuration.file.YamlConfiguration.loadConfiguration(new File(Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("HypeCoreRolePlayCommands")).getDataFolder() + "/config.yml"));
-
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("try")) {
-            if (!(sender instanceof Player p)) {
-                sender.sendMessage(Component.text("Эта команда доступна только для игроков!").color(TextColor.fromHexString("#bf616a")));
-                return true;
-            }
-            final boolean isSuccess = getRandomBoolean();
-            String action = "сделать что-то";
-            if (args.length >= 1) {
-                action = String.join(" ", args);
-            }
-            for (Player near : Bukkit.getOnlinePlayers()) {
-                if (near.getWorld() == p.getWorld() && near.getLocation().distance(p.getLocation()) <= config.getInt("try-radius")) {
-                    near.sendMessage(Component.text(p.getName() + " попытался " + action + ", и у него это ").color(TextColor.fromHexString("#d77bba")).append(
-                            isSuccess ? Component.text("получилось.").color(TextColor.fromHexString("#a3be8c")).decorate(TextDecoration.BOLD) : Component.text("не получилось.").color(TextColor.fromHexString("#bf616a")).decorate(TextDecoration.BOLD)));
-                }
-            }
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage(Component.text("Эта команда доступна только для игроков!").color(TextColor.fromHexString("#bf616a")));
+            return true;
         }
-        return true;
-    }
 
-    private boolean getRandomBoolean() {
-        return Math.random() >= 0.5;
+        Main.sendMessageToNear(
+                Component.text(p.getName() + " попытался " + ((args.length >= 1) ? String.join(" ", args) : "сделать что-то") + ", и у него это ").color(TextColor.fromHexString("#d77bba")).append((Math.random() >= 0.5) ? Component.text("получилось.").color(TextColor.fromHexString("#a3be8c")).decorate(TextDecoration.BOLD) : Component.text("не получилось.").color(TextColor.fromHexString("#bf616a")).decorate(TextDecoration.BOLD)),
+                p,
+                Main.config.getInt("radius.try")
+        );
+        return true;
     }
 }
 
